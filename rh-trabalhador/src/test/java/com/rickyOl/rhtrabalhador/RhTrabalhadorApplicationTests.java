9b0d68bec26a8a4869e3a7336c391e0629deebcf
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-04 18:41:04 -0300

    Projeto RH

---
+++ rh-trabalhador/src/test/java/com/rickyOl/rhtrabalhador/RhTrabalhadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyOl.rhtrabalhador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTrabalhadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
