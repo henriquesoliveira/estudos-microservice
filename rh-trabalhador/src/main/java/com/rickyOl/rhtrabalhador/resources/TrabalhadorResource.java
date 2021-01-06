4640e72d00006374d42520d775037d90a5e7b16c
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-06 14:27:56 -0300

    RH-TRABALHADOR -  CONFIGURAÇÃO SERVER CLIENT

--- rh-trabalhador/src/main/java/com/rickyOl/rhtrabalhador/resources/TrabalhadorResource.java
+++ rh-trabalhador/src/main/java/com/rickyOl/rhtrabalhador/resources/TrabalhadorResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,21 @@ public class TrabalhadorResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(TrabalhadorResource.class);
 	
+	@Value("${test.config}")
+	private String testeConfig;
+	
 	@Autowired
 	private Environment env;
 	
 	@Autowired
 	private TrabalhadorRepositorio repositorio;
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs(){
+	
+		logger.info(testeConfig);
+		return ResponseEntity.noContent().build();
+	} 
 	
 	@GetMapping
 	public ResponseEntity<List<Trabalhador>> buscarTodos(){
