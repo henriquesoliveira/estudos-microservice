220b759390b17b03942c08c44adc43498fbe270d
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-06 09:56:15 -0300

    Criação Projeto Gateway Zuul

---
+++ rh-api-gateway-zuul/src/test/java/com/rickyOl/rhapigatewayzuul/RhApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyOl.rhapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
