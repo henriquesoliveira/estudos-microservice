1757f8bde660b546e2d017e865e3fc8d1b270633
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-06 13:48:43 -0300

    Criação projeto config server

---
+++ rh-config-server/src/test/java/com/rickyOl/rhconfigserver/RhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyOl.rhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
