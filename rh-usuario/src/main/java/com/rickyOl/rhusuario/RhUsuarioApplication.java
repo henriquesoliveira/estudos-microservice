fb3b00d76235071e203d67343825bfd50903b818
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-07 08:35:50 -0300

    Configuração do projeto Usuario

--- rh-usuario/src/main/java/com/rickyOl/rhusuario/RhUsuarioApplication.java
+++ rh-usuario/src/main/java/com/rickyOl/rhusuario/RhUsuarioApplication.java
@@ -2,7 +2,9 @@ package com.rickyOl.rhusuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RhUsuarioApplication {
 
