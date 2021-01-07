9b58ca5d0e27bf7223bd81a045824511637403c5
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-07 08:33:04 -0300

    Criação MS Usuario

---
+++ rh-usuario/src/test/java/com/rickyOl/rhusuario/RhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyOl.rhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
