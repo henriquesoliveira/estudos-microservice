3da01127a916270d3fba0da076afcfbc2448e3d7
henriquesoliveira <oliveira.siqueira@hotmail.com>
2021-01-07 09:15:15 -0300

    Criando database para Usuario e Papel

--- rh-usuario/src/main/java/com/rickyOl/rhusuario/RhUsuarioApplication.java
+++ rh-usuario/src/main/java/com/rickyOl/rhusuario/RhUsuarioApplication.java
@@ -1,15 +1,27 @@
 package com.rickyOl.rhusuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class RhUsuarioApplication {
+public class RhUsuarioApplication implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder pass;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RhUsuarioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("BCRYPT = " + pass.encode("123456"));
+		
 	}
 
 }
